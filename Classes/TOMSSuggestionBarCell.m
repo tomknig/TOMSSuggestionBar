487edd609f1a1cfadf570359f6abc2814bae0bb5
TomKnig <hi@tomknig.de>
2014-06-15 02:30:21 +0200

    added some visual tests

--- Classes/TOMSSuggestionBarCell.m
+++ Classes/TOMSSuggestionBarCell.m
@@ -33,6 +33,22 @@
     self.textLabel.textAlignment = NSTextAlignmentCenter;
     
     [self addSubview:self.textLabel];
+    
+    [self test];
+}
+
+- (void)test
+{
+    self.textLabel.text = @"Swift";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.textLabel.text = @"Swiftilicious";
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            self.textLabel.text = @"delicious";
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self test];
+            });
+        });
+    });
 }
 
 @end
